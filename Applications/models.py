7e664f29b0d6feb7472673f3aa9b29912d2af2bd
Yoseph Radding <raddingy@lykeme.com>
2015-04-13 12:10:04 -0400

    Removed MySql from requirements.txt.

--- Applications/models.py
+++ Applications/models.py
@@ -9,6 +9,10 @@ from django.core.validators import RegexValidator
 class UserType(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL)
     userAffiliation = models.ForeignKey(Sponsor, null=True, blank=True)
+    wantsToMentor = models.BooleanField(default=False)
+    phoneRegex = RegexValidator(regex=r'^\+?1?\d{9,15}$',
+                                message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
+    phoneNumber = models.CharField(validators=[phoneRegex], unique=True, max_length=10, blank=True, null=True)
 
 class School(models.Model):
     name = models.CharField(max_length=100)
@@ -17,9 +21,42 @@ class School(models.Model):
     state = models.CharField(max_length=100)
     country = models.CharField(max_length=100)
 
+class SchoolAbbreviations(models.Model):
+    abbr = models.CharField(max_length=50)
+    school = models.ForeignKey(School)
+
+class Tech(models.Model):
+    name = models.CharField(max_length=100)
+
+class TechToMentor(models.Model):
+    tech = models.ForeignKey(Tech)
+    mentor = models.ForeignKey(UserType)
+    position = models.IntegerField(default=0)
+
+class Hackathon(models.Model):
+    name = models.CharField(max_length=100)
+
+class HackathonToApplicant(models.Model):
+    hackathon = models.ForeignKey(Hackathon)
+    applicant = models.ForeignKey(Applicant)
+
 class Applicant(models.Model):
     user = models.OneToOneField(UserType)
-    phoneRegex = RegexValidator(regex=r'^\+?1?\d{9,15}$',
-                                message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
-    phoneNumber = models.CharField(validators=[phoneRegex], unique=True, max_length=10)
-    
+    DoB = models.DateField()
+    school = models.ForeignKey(School)
+    year_choices = (("FR","Freshman"),
+                    ("SP","Sophomore"),
+                    ("JR","Junior"),
+                    ("SN","Senior"),
+                    ("HS","High School"),
+                    ("GD","Graduate"),
+                    ("OT","Other")
+    )
+    year = models.CharField(max_length=2,choices=year_choices)
+    major = models.CharField(max_length=100)
+    github = models.CharField(max_length=100, blank=True, null=True)
+    website = models.CharField(max_length=100, blank=True, null=True)
+    resume = models.FileField(blank=True,null=True)
+    otherHackathons = models.BooleanField(default=False)
+
+
